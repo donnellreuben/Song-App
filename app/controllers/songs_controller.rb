aef3832eea5d792af39636a0e4fb7ea9b6e73cc4
Donnell Reuben <donnell.reuben@icloud.com>
2023-03-31 12:43:29 -0500

    Added Create Action..make sure in controller the @name is singular and then use the body form in HTTPie to edit items to create a new Song

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,6 +1,22 @@
 class SongsController < ApplicationController
+  protect_from_forgery with: :exception, unless: -> { request.format.json? }  
   def index
     @songs = Song.all
     render :index
+  end
+
+  def first_song
+    @songs = Song.first
+    render json: Song.first
+  end
+
+  def create
+    @song = Song.create(
+      title: params[:title],
+      album: params[:album],
+      artist: params[:artist],
+      year: params[:year],
+    )
+    render :show
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get "/songs" => "songs#index"
+  get "/one-song" => "songs#first_song"
+  post  "/songs" => "songs#create"
 end
