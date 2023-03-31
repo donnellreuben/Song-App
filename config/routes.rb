479a47d36432e97c3bbe3d4219100b37522d6a2b
Donnell Reuben <donnell.reuben@icloud.com>
2023-03-31 12:47:45 -0500

    Add show action.. its songs/id#.json and use GET

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get "/songs" => "songs#index"
   get "/one-song" => "songs#first_song"
   post  "/songs" => "songs#create"
+  get "/songs/:id" => "songs#show"
 end
